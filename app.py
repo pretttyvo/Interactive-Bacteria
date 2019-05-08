a3aae8aac7350a0441e5e84f62197a4a37ad9636
Jamie Vo <vo.thipretty@gmail.com>
2019-05-08 01:33:59 -0500

    Gauge Added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 import pandas as pd
 import numpy as np
+import math
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
--- static/js/app.js
+++ static/js/app.js
@@ -22,8 +22,80 @@ async function buildMetadata(sample) {
       const cell = tbody.append("td");
       cell.text(`${key}: ${data[key]}`);
   }
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  // ----------------------------------------
+
+
+
+  // BONUS: Build the Gauge Chart
+  // buildGauge(data.WFREQ);
+  var washings = data.WFREQ
+  console.log(washings)
+  
+// Enter a speed between 0 and 180
+let level = washings*20;
+
+// Trig to calc meter point
+var degrees = 180 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data3 = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'speed',
+    text: washings,
+    hoverinfo: 'text+name'},
+  { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9, 50/9, 50/9, 50],
+  rotation: 90,
+  text: ['8-9', '7-8', '6-7', '5-6',
+            '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(8, 107, 0, .5)', 'rgba(10, 117, 0, .5)','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                         'rgba(210, 206, 145, .5)', 'rgba(222, 216, 160, .5)','rgba(232, 226, 202, .5)',
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: '<b>Belly Button Washing Frequency</b> <br> Scrubs Per Week',
+  height: 500,
+  width: 500,
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data3, layout);
+
+
+
+// ------------------------------------------
 }
 
 async function buildCharts(sample) {
@@ -77,8 +149,8 @@ async function buildCharts(sample) {
   const pieData = [trace2]
 
   const layout2 = {
-      height: 600,
-      width: 800
+      height: 500,
+      width: 500
   };
 
   Plotly.newPlot("pie", pieData, layout2);
